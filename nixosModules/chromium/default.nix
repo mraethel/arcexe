f37255dd7988776d25f3c36dc9252c2e29504337
mraethel <mraethel@outlook.com>
2024-03-27 05:10:59 +0100

    flake (nixosModules)

---
+++ nixosModules/chromium/default.nix
@@ -0,0 +1,15 @@
+{
+  programs.chromium.ManagedBookmarks = [{
+    name = "arcexe";
+    children = [{
+      name = "Exercises";
+      url = "https://github.com/letoram/arcan/wiki/Exercises";
+    } {
+      name = "API";
+      url = "https://github.com/letoram/arcan/wiki/API";
+    } {
+      name = "API (source)";
+      url = "https://chiselapp.com/user/letoram/repository/arcan/dir?name=doc";
+    }];
+  }];
+}
