7a83c94e45fa86a96d446707d1755e976b49d97c
mraethel <mraethel@outlook.com>
2024-04-03 22:30:45 +0200

    ex4/4 (testing)

--- 4/4/exercise4/exercise4.lua
+++ 4/4/exercise4/exercise4.lua
@@ -1,4 +1,7 @@
 function exercise4()
+    local pbox = color_surface(200,200,255,0,255);
+    move_image(pbox,200,200);
+
     local rbox = color_surface(200,200,255,0,0);
     move_image(rbox,100,100);
     
@@ -8,17 +11,31 @@ function exercise4()
     local gbox = color_surface(200,200,0,255,0);
     local gprops = image_surface_properties(gbox);
     
-    blend_image({rbox,bbox,gbox},1.0,100);
+    blend_image({pbox,rbox,bbox,gbox},1.0,200);
+--  show_image(pbox);
+--  show_image(rbox);
+--  show_image(bbox);
+--  blend_image(gbox,1.0,100);
     
+    link_image(pbox,rbox);
     link_image(rbox,bbox);
     link_image(bbox,gbox);
-   
-    expire_image(gbox,180);
-    move_image(gbox,(VRESW - gprops.width)/2,(VRESH - gprops.height)/2);
-    rotate_image({rbox,gbox},45,200);
 
-    image_mask_clear(rbox,MASK_LIVING);
-    image_mask_clear(bbox,MASK_LIVING);
-    image_mask_clear(rbox,MASK_OPACITY);
+    expire_image(gbox,180);
+--  move_image(gbox,(VRESW - gprops.width)/2,(VRESH - gprops.height)/2);
+    rotate_image({rbox,gbox},45,200);
+--  rotate_image(gbox,45,200);
+
+--  image_mask_clear(pbox,MASK_LIVING);
+--  image_mask_clear(rbox,MASK_LIVING);
+--  image_mask_clear(bbox,MASK_LIVING);
+--  image_mask_clear(gbox,MASK_LIVING);
+--  image_mask_clear(pbox,MASK_OPACITY);
+--  image_mask_clear(rbox,MASK_OPACITY);
+--  image_mask_clear(bbox,MASK_OPACITY);
+--  image_mask_clear(gbox,MASK_OPACITY);
+--  image_mask_clear(pbox,MASK_ORIENTATION);
 --  image_mask_clear(rbox,MASK_ORIENTATION);
+--  image_mask_clear(bbox,MASK_ORIENTATION);
+--  image_mask_clear(gbox,MASK_ORIENTATION);
 end
